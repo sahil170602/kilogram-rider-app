7a393c67fad0fa4b6c22f16ef8f938e3fcf09884
Sahil <your-sahilmeshram1917@gm.com>
2026-03-24 04:38:37 +0530

    feat: initialized rider android project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kilogram.rider',
+  appName: 'Kilogram Rider',
+  webDir: 'dist'
+};
+
+export default config;
